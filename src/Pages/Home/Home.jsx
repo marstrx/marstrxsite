d4e9580864d6740a4569690cd012049f7614a05c
marstrx <marstrone@gmail.com>
2025-11-24 18:48:14 +0100

    fix :full screen removed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import logo from "../../assets/Images/marstrx.webp";
 import linkdinLogo from "../../assets/Images/linkdin.webp";
 import githubLogo from "../../assets/Images/github.webp";
 import xLogo from "../../assets/Images/x.webp";
+import { motion } from "motion/react";
 
 function Home() {
   const gradientMove = keyframes`
@@ -48,7 +49,7 @@ function Home() {
   //  end of animations
 
   return (
-    <div className="bg-gray-800 min-h-screen pt-10 md:pt-20">
+    <div className="bg-gray-800 pt-10 md:pt-20">
       <div className="relative w-full text-white flex items-center justify-center overflow-hidden py-12">
         <div className="relative z-10 flex flex-col md:flex-row items-center justify-center gap-1 md:gap-3 max-w-6xl mx-auto text-center md:text-left px-2">
           <div>
@@ -108,10 +109,16 @@ function Home() {
 
             {/* Buttons */}
             <div className="flex sm:flex-row gap-4 mt-8 justify-center md:justify-start">
-              <a href="#projects" className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg transition duration-300 transform hover:scale-105">
+              <a
+                href="#projects"
+                className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg transition duration-300 transform hover:scale-105"
+              >
                 View my work
               </a>
-              <a href="#contact" className="bg-transparent hover:bg-gray-700 text-white font-semibold py-3 px-6 border border-white rounded-lg transition duration-300 transform hover:scale-105">
+              <a
+                href="#contact"
+                className="bg-transparent hover:bg-gray-700 text-white font-semibold py-3 px-6 border border-white rounded-lg transition duration-300 transform hover:scale-105"
+              >
                 Contact me
               </a>
             </div>
@@ -121,10 +128,19 @@ function Home() {
 
       {/* Description Section */}
       <div>
-        <h2 className="text-lg sm:text-xl md:text-2xl max-w-3xl text-center mx-auto text-gray-200 leading-relaxed">
-          I'm a Full-Stack Web Developer passionate about building clean, fast,
-          and scalable applications using modern web technologies.
-        </h2>
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.4,
+            scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+          }}
+        >
+          <h2 className="text-lg sm:text-xl md:text-2xl max-w-3xl text-center mx-auto text-gray-200 leading-relaxed">
+            I'm a Full-Stack Web Developer passionate about building clean,
+            fast, and scalable applications using modern web technologies.
+          </h2>
+        </motion.div>
       </div>
     </div>
   );

138c97c84b7c54d2f192601c5cee98c948f7688a
marstrx <marstrone@gmail.com>
2025-11-24 19:24:15 +0100

    mail added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -104,6 +104,15 @@ function Home() {
                   alt="X logo"
                 />
               </a>
+
+              {/* Email */}
+              <a
+                href="mailto:marstrx0@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg className="w-10 h-auto md:w-11 transform transition-transform duration-300 hover:scale-110" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path fill="#42a5f5" d="M28 6H4a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h24a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2m0 6l-12 6l-12-6V8l12 6l12-6Z"/></svg>
+              </a>
             </div>
 
             {/* Buttons */}
